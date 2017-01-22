8e4e091eaca6de2426068cc6bbab53ddc177cc68
Radoslav Georgiev <valsr@valsr.com>
2017-01-22 12:31:41 -0500

    Added callbacks

--- com/valsr/psb/sound/Player.py
+++ com/valsr/psb/sound/Player.py
@@ -4,17 +4,22 @@ Created on Jan 17, 2017
 @author: radoslav
 '''
 from gi.repository import Gst
-from com.valsr.psb.sound.Util import PlayerState
+from kivy.clock import Clock
 from kivy.logger import Logger
 import os
-from kivy.clock import Clock
+from time import sleep
+import uuid
+
+from com.valsr.psb.sound.Util import PlayerState
+
 
 _PLAYER_MESSAGE_LIMIT_ = 10
-class Player(object):
+_PLAYER_MESSAGE_TIME_ = 0.5
+class Player( object ):
     '''
     classdocs
     '''
- 
+
     file_ = None
     state_ = PlayerState.NOTINIT
     time_ = 0
@@ -23,135 +28,169 @@ class Player(object):
     source_ = None
     sink_ = None
     id_ = None
-    updatecallbacks_ = []
-    messageCallbacks_ = []
-     
-    def __init__(self, id, file):
+    updateCallbacks_ = {}
+    messageCallbacks_ = {}
+    messageScheduleEvent_ = None
+
+    def __init__( self, id, file ):
         '''
         Constructor
         '''
-        self.file_ = os.path.abspath(file)
+        self.file_ = os.path.abspath( file )
         self.id_ = id
         self._setUpPipeline()
-    
-    def _setUpPipeline(self):
-        self.pipeline_ = Gst.Pipeline.new(self.id_ + '_player')
+
+    def _setUpPipeline( self ):
+        self.pipeline_ = Gst.Pipeline.new( self.id_ + '_player' )
 
         # creating the filesrc element, and adding it to the pipeline
-        self.source_ = Gst.ElementFactory.make("filesrc", self.id_ + "_source")
-        self.source_.set_property("location", self.file_)
-        self.pipeline_.add(self.source_)
-        
+        self.source_ = Gst.ElementFactory.make( "filesrc", self.id_ + "_source" )
+        self.source_.set_property( "location", self.file_ )
+        self.pipeline_.add( self.source_ )
+
         # creating and adding the decodebin element , an "automagic" element able to configure itself to decode pretty much anything
-        self.decode_ = Gst.ElementFactory.make("decodebin", self.id_ + "_decodebin")
-        self.pipeline_.add(self.decode_)
+        self.decode_ = Gst.ElementFactory.make( "decodebin", self.id_ + "_decodebin" )
+        self.pipeline_.add( self.decode_ )
         # connecting the decoder's "pad-added" event to a handler: the decoder doesn't yet have an output pad (a source), it's created at runtime when the decoders starts receiving some data
-        self.decode_.connect("pad-added", self._decodeSrcCreated) 
-        
+        self.decode_.connect( "pad-added", self._decodeSrcCreated )
+
         # setting up (and adding) the alsasin, which is actually going to "play" the sound it receives
-        self.sink_ = Gst.ElementFactory.make("autoaudiosink", self.id_ + "_sink")
-        self.pipeline_.add(self.sink_)
+        self.sink_ = Gst.ElementFactory.make( "autoaudiosink", self.id_ + "_sink" )
+        self.pipeline_.add( self.sink_ )
 
         # linking elements one to another (here it's just the filesrc - > decoder link , the decoder -> sink link's going to be set up later)
-        self.source_.link(self.decode_)
+        self.source_.link( self.decode_ )
         self.state_ = PlayerState.READY
-        
-    def _decodeSrcCreated(self, element, pad):
-        pad.link(self.sink_.get_static_pad('sink'))
-   
-    def registerCallback(self):
+
+    def _decodeSrcCreated( self, element, pad ):
+        pad.link( self.sink_.get_static_pad( 'sink' ) )
+
+    def unregisterMessageLoop( self ):
+        if self.messageScheduleEvent_:
+            self.messageScheduleEvent_.cancel()
+
+    def registerMessageLoop( self ):
+        global _PLAYER_MESSAGE_TIME_
         if self.state_ == PlayerState.PLAYING or self.state_ == PlayerState.PAUSED:
-            Clock.schedule_once(self.messageLoop, 1)
-        
-    def messageLoop(self, delta):
+            self.messageScheduleEvent_ = Clock.schedule_once( self.messageLoop, _PLAYER_MESSAGE_TIME_ )
+        else:
+            self.messageScheduleEvent_ = None
+
+    def messageLoop( self, delta ):
         global _PLAYER_MESSAGE_LIMIT_
         # check for messages
         bus = self.pipeline_.get_bus()
-        
+
         # check upto 10 messages
-        for i in range(0, _PLAYER_MESSAGE_LIMIT_):
+        for i in range( 0, _PLAYER_MESSAGE_LIMIT_ ):
             message = bus.pop()
             if message == None:
                 break
-            
-            self.onMessage(bus, message)
-            for cb in self.messageCallbacks_:
-                if cb(bus, message):
-                    self.registerCallback()
+
+            self.onMessage( bus, message )
+            for key in self.messageCallbacks_:
+                if self.messageCallbacks_[key]( self, bus, message ):
+                    self.registerMessageLoop()
                     return
-        
+
         # call the update callbacks
-        for cb in self.updatecallbacks_:
-            if cb(delta):
-                self.registerCallback()
+        for key in self.updateCallbacks_:
+            if self.updateCallbacks_[key]( self, delta ):
+                self.registerMessageLoop()
                 return
-        
-        self.registerCallback()
-             
-    def onMessage(self, bus, message):
+
+        self.registerMessageLoop()
+
+    def registerUpdateCallback( self, cb ):
+        id = str( uuid.uuid1().int )
+        self.updateCallbacks_[id] = cb
+        Logger.debug( "Registered callback by id %s" % id )
+        return id
+
+    def unregisterUpdateCallback( self, id ):
+        if id in self.updateCallbacks_:
+            del self.updateCallbacks_[id]
+            Logger.debug( "Unregistered callback by id %s" % id )
+        else:
+            Logger.debug( "Unable to unregistered callback: %s not found" % id )
+
+    def registerMessageCallback( self, cb ):
+        id = str( uuid.uuid1().int )
+        self.messageCallbacks_[id] = cb
+        Logger.debug( "Registered callback by id %s" % id )
+        return id
+
+    def unregisterMessageCallback( self, id ):
+        if id in self.messageCallbacks_:
+            del self.messageCallbacks_[id]
+            Logger.debug( "Unregistered callback by id %s" % id )
+        else:
+            Logger.debug( "Unable to unregistered callback: %s not found" % id )
+
+
+    def onMessage( self, bus, message ):
         t = message.type
         if t == Gst.MessageType.EOS:
-            self.onFinish(bus)
+            self.onFinish( bus )
         elif t == Gst.MessageType.ERROR:
-            self.onFinish(bus)
+            self.onFinish( bus )
             self.state_ = PlayerState.ERROR
             error, debug = message.parse_error()
-            self.error_ = "Error %s: %s" % (error, debug)
-     
-    def getState(self):
+            self.error_ = "Error %s: %s" % ( error, debug )
+
+    def getState( self ):
         return self.state_
-     
-    def getLastError(self):
+
+    def getLastError( self ):
         return self.error_
-     
-    def onFinish(self, bus):
+
+    def onFinish( self, bus ):
         self.state_ = PlayerState.STOPPED
-        Logger.debug("Finished playing %s" % self.file_)
-         
-    def play(self):
+        Logger.debug( "Finished playing %s" % self.file_ )
+
+    def play( self ):
         if self.state_ is PlayerState.READY or self.state_ is PlayerState.PAUSED or self.state_ is PlayerState.STOPPED:
-            self.pipeline_.set_state(Gst.State.PLAYING)
-            Logger.debug("Playing %s" % self.file_)
+            self.pipeline_.set_state( Gst.State.PLAYING )
+            Logger.debug( "Playing %s" % self.file_ )
             self.state_ = PlayerState.PLAYING
-            self.registerCallback()
+            self.registerMessageLoop()
         elif self.state_ is PlayerState.PLAYING:
-            Logger.debug("Already playing %s" % self.file_)
+            Logger.debug( "Already playing %s" % self.file_ )
         else:
-            Logger.debug("Not in a state to play - %s" % self.state_.name)
-      
-    def pause(self):
+            Logger.debug( "Not in a state to play - %s" % self.state_.name )
+
+    def pause( self ):
         if self.state_ is PlayerState.PLAYING:
-            self.pipeline_.set_state(Gst.State.PAUSED)
+            self.pipeline_.set_state( Gst.State.PAUSED )
             self.state_ = PlayerState.PAUSED
         else:
-            Logger.debug("Unable to pause since we are not playing")
-               
-    def stop(self):
-        if self.state_ == PlayerState.PLAYING:
-            self.pipeline_.set_state(Gst.State.PAUSED)
-            self.state_ = PlayerState.PAUSED
-         
-        if self.state_ == PlayerState.PAUSED or self.state_ == PlayerState.READY:
-            self.pipeline_.set_state(Gst.State.NULL)
-            self.state_ = PlayerState.STOPPED
-             
-        Logger.debug("Stopped playing %s" % self.file_)
-     
-    def destroy(self):
+            Logger.debug( "Unable to pause since we are not playing" )
+
+    def stop( self, waitForStop = True ):
+        self.pipeline_.set_state( Gst.State.NULL )
+        self.state_ = PlayerState.STOPPED
+
+        self.pipeline_.get_state( Gst.CLOCK_TIME_NONE if waitForStop else 0 )
+
+        self.unregisterMessageLoop()
+
+        Logger.debug( "Stopped playing %s" % self.file_ )
+
+    def destroy( self ):
         self.stop()
         self.pipeline_ = None
-        Logger.debug("Destroyed player (%s)" % self.file_)
+        Logger.debug( "Destroyed player (%s)" % self.file_ )
 
-    def queryPos(self):
+    def queryPos( self ):
         if self.state_ is PlayerState.PAUSED or self.state_ is PlayerState.PLAYING:
-            ret, current = self.pipeline_.query_position(Gst.Format.TIME)
-            return current/Gst.SECOND
-        
+            ret, current = self.pipeline_.query_position( Gst.Format.TIME )
+            return current / Gst.SECOND
+
         return -1
-    
-    def queryTime(self):
+
+    def queryTime( self ):
         if self.state_ is PlayerState.PAUSED or self.state_ is PlayerState.PLAYING or self.state_ is PlayerState.READY or self.state_ is PlayerState.STOPPED:
-            ret, current = self.pipeline_.query_duration(Gst.Format.TIME)
-            return current/Gst.SECOND
-        
+            ret, current = self.pipeline_.query_duration( Gst.Format.TIME )
+            return current / Gst.SECOND
+
         return -1
--- com/valsr/psb/ui/AddSoundDialogue.py
+++ com/valsr/psb/ui/AddSoundDialogue.py
@@ -3,105 +3,108 @@ Created on Jan 14, 2017
 
 @author: radoslav
 '''
-from kivy.lang import Builder
-from com.valsr.psb.ui.WindowBase import WindowBase
-import os
-from com.valsr.psb.sound import PlayerManager
-from kivy.logger import Logger
+from gi.repository import Gst
 from kivy.clock import Clock
-from com.valsr.psb.sound.Util import PlayerState
+from kivy.lang import Builder
+from kivy.logger import Logger
+import os
 
-class AddSoundDialogue(WindowBase):
+from com.valsr.psb.sound import PlayerManager
+from com.valsr.psb.sound.Util import PlayerState
+from com.valsr.psb.ui.WindowBase import WindowBase
+
+
+class AddSoundDialogue( WindowBase ):
     '''
     classdocs
     '''
     cwd_ = os.getcwd()
     file_ = None
     playerId_ = None
-    
-    def init(self, **kwargs):
+
+    def init( self, **kwargs ):
         self.title = "Add Audio File"
         pass
-    
-    def on_open(self, **kwargs):
-        self.getUI('Files').path = self.cwd_
-        self.getUI('PathInput').text = self.cwd_
-        self.getUI('Files').filters.append(self.uiFilterFiles)
-        
-    def createRootUI(self):
-        return Builder.load_file("ui/kv/addsound.kv")
-    
-    def uiAutoplayLabel(self, touch):
-        label = self.getUI('AutoPlayLabel')
-        if label.collide_point(*touch.pos):
-            self.getUI('AutoPlayButton').active = not self.getUI('AutoPlayButton').active
-     
-    def uiCancel(self, *args):
+
+    def on_open( self, **kwargs ):
+        self.getUI( 'Files' ).path = self.cwd_
+        self.getUI( 'PathInput' ).text = self.cwd_
+        self.getUI( 'Files' ).filters.append( self.uiFilterFiles )
+
+    def createRootUI( self ):
+        return Builder.load_file( "ui/kv/addsound.kv" )
+
+    def uiAutoplayLabel( self, touch ):
+        label = self.getUI( 'AutoPlayLabel' )
+        if label.collide_point( *touch.pos ):
+            self.getUI( 'AutoPlayButton' ).active = not self.getUI( 'AutoPlayButton' ).active
+
+    def uiCancel( self, *args ):
         if self.playerId_ is not None:
-            PlayerManager.destroyPlayer(self.playerId_)
-            
+            PlayerManager.destroyPlayer( self.playerId_ )
+
         self.dismiss()
-        
-    def uiOpen(self, *args):
+
+    def uiOpen( self, *args ):
         if self.playerId_ is not None:
-            PlayerManager.destroyPlayer(self.playerId_)
-        
+            PlayerManager.destroyPlayer( self.playerId_ )
+
         self.dismiss()
-    
-    def uiFilterFiles(self, folder, file):
-        if os.path.isdir(file):
+
+    def uiFilterFiles( self, folder, file ):
+        if os.path.isdir( file ):
             return True
-        
-        if self.getUI('PathInput').text is not folder:
-            self.getUI('PathInput').text = folder
-        ext = os.path.splitext(file)[1]
+
+        if self.getUI( 'PathInput' ).text is not folder:
+            self.getUI( 'PathInput' ).text = folder
+        ext = os.path.splitext( file )[1]
         return ext.lower() in self.controller_.getAllowedAudioFiles()
-    
-    def fileSelection(self, *args):
-        files = self.getUI('Files')
-        
+
+    def fileSelection( self, *args ):
+        files = self.getUI( 'Files' )
+
         file = files.selection[0]
-             
-        if os.path.isfile(file):
-            self.autoPlay(file)
+
+        if os.path.isfile( file ):
+            self.autoPlay( file )
             pass
-        
+
         return True
-    
-    def autoPlay(self, file):
-        if self.getUI('AutoPlayButton').active:
+
+    def autoPlay( self, file ):
+        if self.getUI( 'AutoPlayButton' ).active:
             if self.playerId_ is not None:
-                PlayerManager.destroyPlayer(self.playerId_)
-            
-            (id, p) = PlayerManager.createPlayer(file)
+                PlayerManager.destroyPlayer( self.playerId_ )
+
+            ( id, p ) = PlayerManager.createPlayer( file )
             self.playerId_ = id
+            p.registerUpdateCallback( self.updateUI )
+            p.registerMessageCallback( self.messageCallback )
             p.play()
-            Clock.schedule_once(self.updateUI, 1)
-            
+
             # load wave FORM
             # play file
-            
-    def updateUI(self, delta):
-        if self.playerId_:
-            p = PlayerManager.getPlayer(self.playerId_)
-            dur = p.queryTime()
-            pos = p.queryPos()
-            
-            Logger.debug(str(pos) + "/" + str(dur))
-            Logger.debug(p.getState().name)
-            if p.getState() is PlayerState.READY:
-                PlayerManager.destroyPlayer(self.playerId_)
-                self.playerId_ = None
-            Clock.schedule_once(self.updateUI, 1)
-            
-    def onStop(self):
+
+    def messageCallback( self, player, bus, message ):
+        if message.type == Gst.MessageType.EOS:
+            print( "blha" )
+            self.onStop()
+
+    def updateUI( self, player, delta ):
+        dur = player.queryTime()
+        pos = player.queryPos()
+
+        Logger.debug( player.getState().name )
+        Logger.debug( str( pos ) + "/" + str( dur ) )
+
+    def onStop( self ):
         if self.playerId_ is not None:
-            p = PlayerManager.getPlayer(self.playerId_)
+            p = PlayerManager.getPlayer( self.playerId_ )
             p.stop()
-    
-    def onPlay(self):
+
+    def onPlay( self ):
         if self.playerId_ is not None:
-            p = PlayerManager.getPlayer(self.playerId_)
+            p = PlayerManager.getPlayer( self.playerId_ )
             if p.getState() == PlayerState.PLAYING:
                 p.pause()
             else:
